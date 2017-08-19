4a2feb25750c530f4e68d9c510359d2a81760443
Yifan Gu <gyf304@users.noreply.github.com>
2017-08-19 14:04:04 -0400

    fixing iOS web app scroll bug

--- js/index.js
+++ js/index.js
@@ -6,47 +6,6 @@ occupancy.data = {
   infoCards: occupancy.config.infoCards.slice()
 };
 
-occupancy.app = new Vue({
-  el: '#app',
-  data: occupancy.data,
-  methods: {
-    'moment': moment
-  }
-})
-
-if (/iPad|iPhone|iPod/.test(window.navigator.userAgent) && !window.MSStream && window.navigator.standalone) {
-  document.body.className += ' web-app';
-  window.addEventListener('touchmove', function(){});
-  var html = document.getElementsByTagName('html')[0];
-  var wrapper = document.getElementById('content-wrapper');
-  var prevDef = function(e){e.preventDefault();};
-  var stopProp = function(e){e.stopPropagation();e.cancelBubble=true;};
-  html.ontouchmove = prevDef;
-  html.ontouchcancel = prevDef;
-  html.ontouchstart = prevDef;
-  html.ontouchend = prevDef;
-  wrapper.ontouchmove = stopProp;
-  wrapper.ontouchcancel = stopProp;
-  wrapper.ontouchstart = stopProp;
-  wrapper.ontouchend = stopProp;
-}
-
-if (/iPad|iPhone|iPod/.test(window.navigator.userAgent) && !window.MSStream && !window.navigator.standalone) {
-  // iOS detection from: http://stackoverflow.com/a/9039885/177710
-  occupancy.data.infoCards.push({
-    title: 'Install as iOS App', 
-    contentHtml: '<p>You can install Occupancy as an app:</p>' +
-    '<p> 1. Tap share in bottom bar' +
-    '<p> 2. Tap "Add to Home Screen"'
-  });
-}
-
-$(document).ready(function(){
-  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-  $('.modal').modal();
-  FastClick.attach(document.body);
-});
-
 occupancy.populateLocations = function(){
   return axios.get(occupancy.config.server + '/api/v1/locations/')
   .then(function(response){
@@ -64,7 +23,35 @@ occupancy.updateLocations = function(){
   }
 };
 
-occupancy.populateLocations()
-.then(occupancy.updateLocations);
+occupancy.app = new Vue({
+  el: '#app',
+  data: occupancy.data,
+  methods: {
+    'moment': moment
+  }
+})
 
-window.setInterval(occupancy.updateLocations, occupancy.config.updateInterval * 1000);
+window.onload = function(){
+  // iOS detected, iOS detection from: http://stackoverflow.com/a/9039885/177710
+  if (/iPad|iPhone|iPod/.test(window.navigator.userAgent) && !window.MSStream) {
+    if (window.navigator.standalone) { // web app
+      document.getElementById('app').className += ' web-app';
+      iNoBounce.enable();
+    } else { // not web app, add install message
+      occupancy.data.infoCards.push({
+        title: 'Install as iOS App', 
+        contentHtml: '<p>You can install Occupancy as an app:</p>' +
+        '<p> 1. Tap share in bottom bar' +
+        '<p> 2. Tap "Add to Home Screen"'
+      });
+    }
+  }
+  
+  $('.modal').modal();
+  FastClick.attach(document.body);
+
+  occupancy.populateLocations()
+  .then(occupancy.updateLocations);
+  
+  window.setInterval(occupancy.updateLocations, occupancy.config.updateInterval * 1000);
+};
