1b14884efca321f40ad05e8b869567e6c5156c4f
Yifan Gu <gyf304@users.noreply.github.com>
2018-01-18 03:35:00 +0800

    iPhone X support

--- js/index.js
+++ js/index.js
@@ -31,12 +31,23 @@ occupancy.app = new Vue({
   }
 });
 
+function iOSVersion() {
+  if (/iP(hone|od|ad)/.test(navigator.platform)) {
+    // supports iOS 2.0 and later: <http://bit.ly/TJjs1V>
+    var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+    return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+  }
+}
+
 window.onload = function(){
   // iOS detected, iOS detection from: http://stackoverflow.com/a/9039885/177710
   if (/iPad|iPhone|iPod/.test(window.navigator.userAgent) && !window.MSStream) {
     if (window.navigator.standalone) { // web app
       document.getElementById('app').className += ' web-app';
-      iNoBounce.enable();
+      var version = iOSVersion();
+      if (version && version[0] < 11) {
+        iNoBounce.enable();
+      }
     } else { // not web app, add install message
       occupancy.data.infoCards.push({
         title: 'Install as iOS App', 
